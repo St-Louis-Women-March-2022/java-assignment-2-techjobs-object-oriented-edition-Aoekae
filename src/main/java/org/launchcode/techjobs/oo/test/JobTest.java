31dd6f15f21e7abe73aec0ce9b817a49db813494
Aoekae <dark_aoekae@yahoo.com>
2022-09-07 09:55:16 -0500

    finished up testJobsForEquality()

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -37,4 +37,12 @@ public class JobTest {
         assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
 
     }
+
+    @Test
+    public void testJobsForEquality() {
+        Job testJob1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job testJob2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertFalse(testJob1.equals(testJob2));
+    }
 }
