626a6ac52e969ff8adda772d389bfcf1d5694720
Aoekae <dark_aoekae@yahoo.com>
2022-09-20 21:46:17 -0500

    Created JobField abstract class, extended into the other classes.

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -1,5 +1,58 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public abstract class JobField {
+
+    //fields
+    private int id;
+    private static int nextId = 1;
+
+    private String value;
+
+    //constructors
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+
+    //custom methods
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Location)) return false;
+        Location location = (Location) o;
+        return getId() == location.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    //getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 
 }
